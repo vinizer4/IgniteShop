6eeef68b57c8dab97602667e2621c2dec4fe1694
ViniciusTS <vinicius.ts.online@gmail.com>
2022-12-26 21:07:31 -0300

    feat: Finished Card Challenge

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,20 +1,32 @@
-import { globalCss } from ".";
+import { createGlobalStyle } from 'styled-components';
+import 'react-toastify/dist/ReactToastify.css';
 
-export const globalStyles = globalCss({
-  '*': {
-    margin: 0,
-    padding: 0,
-    boxSizing: 'border-box',
-  },
+import background from '../assets/images/background.svg';
 
-  body: {
-    backgroundColor: '$gray900',
-    color: '$gray100',
-    '-webkit-font-smoothing': 'antialiased',
-  },
-
-  'body, input, textarea, button': {
-    fontFamily: 'Roboto',
-    fontWeight: 400,
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
   }
-})
+
+  body {
+    background: #191920 url(${background}) no-repeat center top;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, button {
+    font: 14px Roboto, sans-serif;
+  }
+
+  #root {
+    max-width: 1020px;
+    margin: 0 auto;
+    padding: 0 20px 50px;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
